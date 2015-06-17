7e8b5e549c4a0e67a4989a48410aa6b5f3522772
Mike <mcwebmike@gmail.com>
2015-06-17 13:42:17 -0500

    added account type and currency

--- gvi/accounts/models.py
+++ gvi/accounts/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
 
-class Account(models.Model):
+class Currency(models.Model):
     name = models.CharField(max_length=25)
-    number = models.CharField(max_length=140)
-    balance = models.DecimalField(decimal_places=10, max_digits=19)
 
     def __str__(self):
         return self.name
 
 
-class Currency(models.Model):
+class Account(models.Model):
+    BANK = 'b'
+    CASH = 'c'
+    TYPE_CHOICES = (
+        (BANK, 'Bank'),
+        (CASH, 'Cash'),
+    )
+    type = models.CharField(max_length=5, choices=TYPE_CHOICES, default=CASH)
     name = models.CharField(max_length=25)
+    number = models.CharField(max_length=140)
+    balance = models.DecimalField(decimal_places=10, max_digits=19)
+    currency = models.ForeignKey(Currency)
 
     def __str__(self):
         return self.name
