730f83c82522c9f8f01a41aee219d29638d0d20d
Vidal Chavez <vidal.gk@gmail.com>
2015-06-22 15:56:47 -0500

    Fix error in accounts/models.py

--- gvi/accounts/models.py
+++ gvi/accounts/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-import datetime
-
 
 class Currency(models.Model):
     name = models.CharField(max_length=25)
-    contraction = models.CarField(max_length=5)
+    contraction = models.CharField(max_length=5)
     def __str__(self):
         return self.name
 
@@ -22,22 +20,22 @@ class Account(models.Model):
     number = models.CharField(max_length=140, blank=True)
     balance = models.DecimalField(decimal_places=10, max_digits=19, default=0)
     currency = models.ForeignKey(Currency, default=DEFAULT_CURRENCY_ID)
-    active = models.BooleanField(initial=True)
+    active = models.BooleanField(default=True)
     #add the account owner
     
     def __str__(self):
         if self.account_type == 'b':
             return self.number
-        else
+        else:
             return self.currency 
 
 
 class Transfer(models.Model):
-    from_account =  models.ForeignKey(Account)
-    to_account = models.ForeignKey(Account)
+    from_account =  models.ForeignKey(Account, related_name="from_account")
+    to_account = models.ForeignKey(Account, related_name="to_account")
     amount = models.DecimalField(decimal_places=10, max_digits=19)
     exchange_rate = models.DecimalField(decimal_places=10, max_digits=19)
-    date = models.DateTimeField(default=datetime.now())
+    date = models.DateTimeField()
 
-     def __str__(self):
-         return self.amount
+    def __str__(self):
+        return self.amount
