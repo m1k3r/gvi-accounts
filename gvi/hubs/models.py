272099379ea09e61ac24c8c01db4433ef75435f4
Vidal Chavez <vidal.gk@gmail.com>
2015-06-24 15:34:24 -0500

    Create hub models

--- gvi/hubs/models.py
+++ gvi/hubs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Hubs(models.Model):
+    HUB = 'h'
+    C_MANAGER = 'c'
+    TYPE_CHOICES = (
+            (HUB, 'Hub'),
+            (C_MANAGER, 'CM'),
+    )
+    hub_type = models.CharField(maxlength=5, choices=TYPE_CHOICES, default=HUB)
+    name = models.CharField(max_length=100)
+    manager = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    active = models.BooleanField(defoult=True)
+    #Hub area ?
