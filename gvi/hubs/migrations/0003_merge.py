a94ead3112f1e2a3a54537b80b11c1d59c65bc4e
Mike <mcwebmike@gmail.com>
2015-06-25 10:31:38 -0500

    new migrations

---
+++ gvi/hubs/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hubs', '0002_auto_20150625_0219'),
+        ('hubs', '0002_auto_20150625_0137'),
+    ]
+
+    operations = [
+    ]
