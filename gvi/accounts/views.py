45b82727cbcbd9bf34f59da7c127ff24166ebd6b
Mike <mcwebmike@gmail.com>
2015-06-25 11:41:27 -0500

    View loads Currencies

--- gvi/accounts/views.py
+++ gvi/accounts/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.core import serializers
 
-from .models import Account
+from .models import Account, Currency
 
 
 def index(request):
     bank_acc = Account.objects.filter(account_type='b')
     cash_acc = Account.objects.filter(account_type='c')
+    curr = Currency.objects.all()
     context = {'bank_acc': bank_acc,
                'cash_acc': cash_acc,
+               'curr': curr,
                }
     # return HttpResponse("Welcome to Accounts Dashboard")
     return render(request, 'accounts/dashboard.html', context)
