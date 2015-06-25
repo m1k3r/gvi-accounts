441801b4666dda5e87e7f912a6f397b9c5bac525
Mike <mcwebmike@gmail.com>
2015-06-24 20:12:31 -0500

    Creates new_account view that handles ajax request, modifies index view to load the template

--- gvi/accounts/views.py
+++ gvi/accounts/views.py
@@ -1,6 +1,26 @@
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from django.core import serializers
+
+from .models import Account
 
 
 def index(request):
-    return HttpResponse("Welcome to Accounts Dashboard")
+    bank_acc = Account.objects.filter(account_type='b')
+    cash_acc = Account.objects.filter(account_type='c')
+    context = {'bank_acc': bank_acc,
+               'cash_acc': cash_acc,
+               }
+    # return HttpResponse("Welcome to Accounts Dashboard")
+    return render(request, 'accounts/dashboard.html', context)
+
+
+def new_account(request):
+    if request.is_ajax:
+        for obj in serializers.deserialize("json", request.body):
+            print obj
+
+        return JsonResponse({'code': '200'})
+    else:
+        return HttpResponseBadRequest()
