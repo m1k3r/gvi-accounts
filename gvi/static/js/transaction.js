e7bf9280eece9a284ce979da2636edb344a0995f
Daniel <dancassig@gmail.com>
2015-06-30 15:28:49 -0500

    Adds enable/disable functionality for Category and Subcategory

--- gvi/static/js/transaction.js
+++ gvi/static/js/transaction.js
@@ -1,3 +1,33 @@
 /**
  * Created by daniel on 30/06/15.
  */
+$(document).ready(function() {
+    $(window).load(function() {
+
+        var lockCategory = function () {
+            if ($("#enableCategory").is(":checked")) {
+                $('#categorySelect').prop('disabled', false);
+            }
+            else {
+                $('#categorySelect').prop('disabled', 'disabled');
+            }
+        };
+
+        var lockSubcategory = function () {
+            if ($("#enableSubcategory").is(":checked")) {
+                $('#subcategorySelect').prop('disabled', false);
+            }
+            else {
+                $('#subcategorySelect').prop('disabled', 'disabled');
+            }
+        };
+
+
+        $(lockCategory);
+        $("#enableCategory").change(lockCategory);
+        $(lockSubcategory);
+        $("#enableSubcategory").change(lockSubcategory);
+
+
+    });
+});
