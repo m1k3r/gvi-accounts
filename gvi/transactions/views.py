be55310e97814260dc6bbeeacc034534b58fc0bd
Daniel <dancassig@gmail.com>
2015-06-30 13:47:00 -0500

    Creates reference to /transactions/. Sends database information to fill transaction table

--- gvi/transactions/views.py
+++ gvi/transactions/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 from django.http import HttpResponse
+from .models import Transaction, Category, Subcategory
+
 
 def index(request):
-    return HttpResponse("Welcome to Transactions Dashboard")
+    transactions = Transaction.objects.order_by('date')
+    category = Category.objects.all()
+    subcategory = Subcategory.objects.all()
+
+    context = {'transactions':transactions,
+               'category':category,
+               'subcategory':subcategory,
+               }
+
+# return HttpResponse("Welcome to Transactions Dashboard")
+    return render(request, 'transactions/dashboard.html', context)
