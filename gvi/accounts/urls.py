c398f18175bbe266ef1e7769b80376538e5e83c1
Mike <mcwebmike@gmail.com>
2015-06-30 15:26:01 -0500

    Creates new url to handle updates and deletes

--- gvi/accounts/urls.py
+++ gvi/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^create_account/$', views.account_api, name='new_account'),
+    url(r'^create_account/$', views.account_new_get, name='new_account'),
+    url(r'^change_account/$', views.account_update_delete, name='update_account'),
 ]
