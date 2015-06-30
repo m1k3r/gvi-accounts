ce0911ed69d1b9ff494a11b62a30c77a03977b54
Daniel <dancassig@gmail.com>
2015-06-30 13:43:18 -0500

    Adds Transaction, Category and Subcategory to the admin app

--- gvi/transactions/admin.py
+++ gvi/transactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Transaction, Category, Subcategory
+
+admin.site.register(Transaction)
+admin.site.register(Category)
+admin.site.register(Subcategory)
