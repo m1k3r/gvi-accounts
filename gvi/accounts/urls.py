129e5849228a6478f2d10b653123cfb3a0a69d69
Mike <mcwebmike@gmail.com>
2015-07-01 10:04:47 -0500

    Adds currency to urls

--- gvi/accounts/urls.py
+++ gvi/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^create_account/$', views.account_new_get, name='new_account'),
     url(r'^change_account/$', views.account_update_delete, name='update_account'),
     url(r'^money_transfer/$', views.money_transfer, name='money_transfer'),
+    url(r'^currencies/$', views.currency_dash, name='currencies'),
 ]
