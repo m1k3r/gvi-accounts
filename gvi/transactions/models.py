74c9f958f38db9c4c79a43bfffb522ebc79d60bb
Daniel <dancassig@gmail.com>
2015-07-01 19:56:08 -0500

    Creates classmethods in Transaction to obtain the year range needed for transaction search functionality

--- gvi/transactions/models.py
+++ gvi/transactions/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.utils import timezone
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
     number = models.CharField(max_length=50, unique=True)
-    
+
     def __str__(self):
         return self.number
 
@@ -20,8 +22,8 @@ class Transaction(models.Model):
     IN = 'i'
     OUT = 'o'
     TYPE_CHOICES = (
-            (IN, 'Money In'),
-            (OUT, 'Money Out'),
+        (IN, 'Money In'),
+        (OUT, 'Money Out'),
     )
     transaction_type = models.CharField(max_length=5, choices=TYPE_CHOICES, default=OUT)
     category = models.ForeignKey(Category)
@@ -35,3 +37,20 @@ class Transaction(models.Model):
     def __str__(self):
         return self.transaction_type + str(self.amount)
 
+    @classmethod
+    def first_year(cls):
+        years = cls.objects.order_by('date')[0].date.year
+        return years
+
+    @classmethod
+    def year_range(cls):
+        year_range = [cls.first_year()]
+
+        if timezone.now().year == year_range:
+            return year_range
+        else:
+            while year_range[-1] != timezone.now().year:
+                year_range.append(year_range[-1] + 1)
+            return year_range
+
+        return 'Critical Error'
