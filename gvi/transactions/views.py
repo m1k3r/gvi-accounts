4910f152a13e978fc3309f26de55910546d45118
Vidal Chavez <vidal.gk@gmail.com>
2015-07-02 20:58:13 -0500

    Invert the order of the querie result

--- gvi/transactions/views.py
+++ gvi/transactions/views.py
@@ -9,7 +9,7 @@ from .models import Transaction, Category, Subcategory
 import datetime
 
 def index(request):
-    transactions = Transaction.objects.order_by('date')
+    transactions = Transaction.objects.order_by('-date')
     category = Category.objects.all()
     subcategory = Subcategory.objects.all()
     year_range = Transaction.year_range()
@@ -27,7 +27,7 @@ def search_transactions(request):
     print "**********"
     print request.body
     print "**********"
-    transactions = Transaction.objects.filter(category__number='5010').order_by('date')
+    transactions = Transaction.objects.filter(category__number='5010').order_by('-date')
     enableCategory = request.POST.get('enableCategory')
     enableSubcategory = request.POST.get('enableSubcategory')
     if enableCategory and enableSubcategory:
@@ -35,18 +35,18 @@ def search_transactions(request):
         subcategorySelect = request.POST.get('subcategorySelect')
         fromDate = request.POST.get('fromDate')
         toDate = request.POST.get('toDate')
-        transactions = Transaction.objects.filter(category__name=categorySelect).filter(subcategory__name=subcategorySelect).filter(date__range=[fromDate, toDate]).order_by('date')
+        transactions = Transaction.objects.filter(category__name=categorySelect).filter(subcategory__name=subcategorySelect).filter(date__range=[fromDate, toDate]).order_by('-date')
     
     elif enableCategory:
         fromDate = request.POST.get('fromDate')
         toDate = request.POST.get('toDate')
         categorySelect = request.POST.get('categorySelect')
-        transactions = Transaction.objects.filter(category__name=categorySelect).filter(date__range=[fromDate, toDate]).order_by('date')
+        transactions = Transaction.objects.filter(category__name=categorySelect).filter(date__range=[fromDate, toDate]).order_by('-date')
     
     else:
         fromDate = request.POST.get('fromDate')
         toDate = request.POST.get('toDate')
-        transactions = Transaction.objects.filter(date__range=[fromDate, toDate]).order_by('date')
+        transactions = Transaction.objects.filter(date__range=[fromDate, toDate]).order_by('-date')
 
     year_range = Transaction.year_range()
     category = Category.objects.all()
