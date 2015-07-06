5aea0320718b61a7cb75cd955b7f78beb9597880
Ana <anaeuriarte@hotmail.com>
2015-07-06 18:53:35 -0500

    Create edit modal

--- gvi/static/js/hubs.js
+++ gvi/static/js/hubs.js
@@ -66,7 +66,7 @@ $(document).on('click', '#hubSave' ,function () {
 function jsonAddHub(json){
 
     $.ajax({
-        url : "create_hub/", // the endpoint
+        url : "hub/", // the endpoint
         type : "POST", // http method
         data : json, // data sent with the post request
         dataType: 'json',
@@ -110,3 +110,125 @@ $(document).on('click', '#addHubCancel' ,function () {
 
 });
 //***************************************** ENDS ADD HUB ***************************************************************
+
+//******************************************* EDIT HUB *****************************************************************
+
+//Function to fill in information in the Edit Hub modal
+//Receives the hub id, calls getJson() function that returns a json with the hub info and fills in the modal fields
+function editHub(id){
+
+    //Get the id from the object
+    id = id.attr('id');
+    
+    //Get json form backend
+    var response = getJson(id);
+
+    response.done(function(data){
+        var name = data.name;
+        var manager = data.manager;
+        var country = data.country;
+
+        $("#inputNameEdit").val(name);
+        $("#inputManagerEdit").val(manager);
+        $("#inputCountryEdit").val(country);
+
+    }).fail(function (json) {
+        alert("Error 1. Can't reach the server.");
+        $('#modalEditHub').modal('hide');
+        return;
+    });
+
+}
+
+//Function that receives a hub id, connects to the backend and return a json with the hub info
+function getJson(id){
+
+    //Create json with the account id
+    var lejson = {'id': id };
+
+    var jxhr = $.ajax({
+        url : "create_hub/", // the endpoint
+        type : "GET", // http method
+        data : lejson, // data sent with the post request
+
+        // handle a successful response
+        success : function(jsonResponse) {
+
+        },
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+            alert("Error connecting to the server. Please try again or contact the system manager.");
+
+        }
+    });
+
+    return jxhr;
+
+}
+
+//Function triggered by SAVE on the Edit Hub modal
+//Validates fields, creates json and calls updateJson() function
+$(document).on('click', '#modal_edit' ,function () {
+
+    //Get data
+    var id = $("#idHubEdit").val();
+    var name = $("#inputNameEdit").val();
+    var manager = $("#inputManagerEdit").val();
+    var country = $("#inputCountryEdit").val();
+
+
+    //Validations
+    if(isNull(name)==false){
+        alert("You need to enter a name.");
+        return;
+    }
+    if(isNull(manager)==false){
+        alert("You need to enter a manager.");
+        return;
+    }
+    if(isNull(country)==false){
+        alert("You need to enter a country.");
+        return;
+    }
+
+    //Connection to backend
+    var hubData = {"id":id, "name": name, "manager":manager, "country":country};
+    updateJson(hubData);
+
+});
+
+//Function that receives a json with the Account info and connects to the backend to edit the account
+function updateJson(json){
+
+    $.ajax({
+        url : "change_hub/", // the endpoint
+        type : "POST", // http method
+        data : json, // data sent with the post request
+
+        // handle a successful response
+        success : function(jsonResponse) {
+
+            // Hides the modal, updates the tables DOM and cleans modal fields
+            $('#modalEditHub').modal('hide');
+            $('#modalEditHub').find('#inputNameEdit').val('');
+            $('#modalEditHub').find('#inputManagerNoEdit').val('');
+            $('#modalEditHub').find('#inputCountryEdit').val('');
+
+            location.reload();
+
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg,err) {
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+            alert("Error editing the hub. Please try again or contact the system manager.");
+
+        }
+    });
+}
+
+//********************************************* ENDS EDIT HUB **********************************************************
+
+
