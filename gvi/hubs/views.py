f145706b70f0cdbd1ab916884fbf3a9d2e7aabf1
Mike <mcwebmike@gmail.com>
2015-07-06 17:52:10 -0500

    Adds index, add_delete_hub, hub_detail view and urls

--- gvi/hubs/views.py
+++ gvi/hubs/views.py
@@ -6,3 +6,11 @@ def index(request):
     hubs = Hubs.objects.all()
     context = {'hubs': hubs}
     return render(request, 'hubs/dashboard.html', context)
+
+def add_delete_hub(request):
+    pass
+
+def hub_detail(request, pk):
+    response = "You are at: " + pk
+    return HttpResponse(response)
+
