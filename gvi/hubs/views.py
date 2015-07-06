a8babd9205d04a304b5df9b9881f86b49b4d40c6
Mike <mcwebmike@gmail.com>
2015-07-06 17:55:51 -0500

    Adds hub detail context

--- gvi/hubs/views.py
+++ gvi/hubs/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from .models import Hubs
 
@@ -7,10 +8,11 @@ def index(request):
     context = {'hubs': hubs}
     return render(request, 'hubs/dashboard.html', context)
 
+@csrf_exempt
 def add_delete_hub(request):
     pass
 
 def hub_detail(request, pk):
-    response = "You are at: " + pk
-    return HttpResponse(response)
-
+    hub = get_object_or_404(Hubs, pk)
+    context = {'hub': hub}
+    return render(request, 'hubs/detail.html', context)
