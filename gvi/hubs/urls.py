cc70bf5fe6f84429a958139ed0b02f59a137b595
Mike <mcwebmike@gmail.com>
2015-07-06 18:01:02 -0500

    updates create_hub url

--- gvi/hubs/urls.py
+++ gvi/hubs/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^hub/$', views.add_delete_hub, name='hub'),
+    url(r'^create_hub/$', views.add_delete_hub, name='hub'),
     url(r'^hub/(?P<pk>\d+)/$', views.hub_detail, name='hub_detail'),
 ]
