f85945d4a77de85556b3c5e21249b0d9a2eb10d0
Mike <mcwebmike@gmail.com>
2015-07-09 14:55:05 -0500

    Renders search results into template

--- gvi/hubs/views.py
+++ gvi/hubs/views.py
@@ -103,25 +103,25 @@ def hub_search(request):
         search_text = request.POST['search_txt']
         if option == 'hub':
             try:
-                hub = Hubs.objects.get(name__contains=search_text)
-                context = {'hub': hub}
+                hubs = Hubs.objects.filter(name__contains=search_text)
+                context = {'hubs': hubs}
             except Hubs.DoesNotExist as e:
                 print "Hubs.DoesNotExist at hub_search POST" + e.args
-                # context = {'error': 'Hub not found'}
-                return HttpResponse("Hub not found")
+                context = {'error': 'Hub not found'}
+                return render(request, 'hubs/dashboard.html', context)
 
-            return HttpResponse(hub.name)
+            return render(request,'hubs/dashboard.html', context)
 
         if option == 'manager':
             try:
-                hub = Hubs.objects.get(manager__contains=search_text)
-                context = {'hub': hub}
+                hubs = Hubs.objects.filter(manager__contains=search_text)
+                context = {'hubs': hubs}
             except Hubs.DoesNotExist as e:
                 print "Hubs.DoesNotExist at hub_search POST" + e.args
                 # context = {'error': 'Hub not found'}
-                return HttpResponse("Hub not found")
+                return render(request, 'hubs/dashboard.html', context)
 
-            return HttpResponse(hub.manager)
+            return render(request, 'hubs/dashboard.html', context)
         else:
             print "Nor HUB nor MANAGER"
             return HttpResponse('Error')
