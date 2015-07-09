8bf1916e7b22983a5ba71a5019beb39a91f2e85d
Vidal Chavez <vidal.gk@gmail.com>
2015-07-09 12:50:41 -0500

    Update views

--- gvi/budgets/views.py
+++ gvi/budgets/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Budgets Dashboard")
+    return render(request, 'budgets/dashboard.html')
