630ece203e7e4b9f20ee02823ed9db27530d0542
Vidal Chavez <vidal.gk@gmail.com>
2015-07-15 16:07:27 -0500

    Update budgets/forms

--- gvi/budgets/forms.py
+++ gvi/budgets/forms.py
@@ -1,11 +1,9 @@
 from django import forms
 
 
-    class BudgetForm(forms.Form):
-        """
-        Form for individual user links
-        """
-        category = forms.CharField()
-        amount = forms.CharField()
-        currency = forms.CharField()
+    
+class BudgetForm(forms.Form):
+    category = forms.CharField()
+    amount = forms.CharField()
+    currency = forms.CharField()
 
