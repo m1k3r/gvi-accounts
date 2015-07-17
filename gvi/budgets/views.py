0513a8311c3d05cee1a856851d4246d1f1b2548b
Vidal Chavez <vidal.gk@gmail.com>
2015-07-16 20:56:13 -0500

    Save the new budget element and the new budget

--- gvi/budgets/views.py
+++ gvi/budgets/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.forms.formsets import formset_factory
 
 from .models import BudgetElement, Budget
 from accounts.models import Currency
 from transactions.models import Category
-from .forms import BudgetForm
+from hubs.models import Hubs
+
+import hashlib
+import time
+import datetime
 
 
+#{{{Index
 def index(request):
     category = Category.objects.all()
     currency = Currency.objects.all() 
@@ -17,34 +22,72 @@ def index(request):
             }
     
     return render(request, 'budgets/dashboard.html', context)
+#}}} 
 
 
+#{{{Create Budget
 def create_budget(request):
-    BudgetFormSet = formset_factory(BudgetForm)
 
-    if request.method == 'POST':
-        budget_formset = BudgetFormSet(request.POST)
-        
-        if budget_form.is_valid():
-            new_budget = []
-            
-            for budget_form in budget_formset:
-                category = budget_form.cleaned_data.get('category')
-                amount = budget_form.cleaned_data.get('amount')
-                currency = budget_form.cleaned_data.get('currency')
+    print "******* FOR ***"
+    print request.body
+    AMOUNT = 0
+    CATEGORY = 1
+    CURRENCY = 2
 
-                """
-                if anchor and url:
-                    new_budget.append(BudgetElement(category=category, amount=amount, currency=currency))
-                """
-                print "**********"
-                print category
-                print amount
-                print currency
-                print "**********"
+    form_elements = []
+    counter = 0
+    
+    hash = hashlib.sha1()
+    hash.update(str(datetime.datetime.now()))
+    BUDGET_NUMBER = hash.hexdigest()
+
+    hub = Hubs.objects.all()[0]
+    
+    new_budget = Budget(number=BUDGET_NUMBER,
+                        initial_date=request.POST['newBudgetFromDate'],
+                        final_date=request.POST['newBudgetToDate'],
+                        hub=hub)
+    
+    new_budget.save()
+
+    for key in request.POST:
+        if "form" in key:
+            form_elements.append(key)
+            print key + ':' + request.POST[key]
+    
+    print len(form_elements)
+    
+    sort_form_elements = sorted(form_elements)
+     
+    while counter < len(form_elements):
+        print sort_form_elements[counter]
+        category = Category.objects.filter(name=request.POST[sort_form_elements[counter+CATEGORY]])[0]
+        currency = Currency.objects.filter(contraction=request.POST[sort_form_elements[counter+CURRENCY]])[0]
+        new_budget_element = BudgetElement(amount=request.POST[sort_form_elements[counter+AMOUNT]],
+                                           category=category,
+                                           currency=currency,
+                                           number=new_budget,
+                                           budget_type='f')
+        new_budget_element.save()
+        counter += 3
+    
+
+    print "******* END ***"
+   
+
+
+    category = Category.objects.all()
+    currency = Currency.objects.all()
+
+    context = {'category': category,
+            'currency':currency,
+            }   
 
     return render(request, 'budgets/dashboard.html', context)
+#}}}
 
+
+#{{{Variable
 def variable(request):
     category = Category.objects.all()
     currency = Currency.objects.all()
@@ -52,4 +95,6 @@ def variable(request):
     context = {'category': category,
             'currency':currency,
             }
+    
     return render(request, 'budgets/variable.html', context)
+#}}}
