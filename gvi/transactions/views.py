e09c60c830ebf6b34419846ee9288cd837189ea3
Mike <mcwebmike@gmail.com>
2015-07-16 21:04:41 -0500

    Adds transactions new, edit, update, delete view

--- gvi/transactions/views.py
+++ gvi/transactions/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404, JsonResponse
 
 from .models import Transaction, Category, Subcategory
 from accounts.models import Account, Currency
+
+from decimal import *
 
 
 def index(request):
@@ -38,14 +41,14 @@ def search_transactions(request):
         transactions = Transaction.objects.filter(category__name=categorySelect)
         transactions = transactions.filter(subcategory__name=subcategorySelect)
         transactions = transactions.filter(date__range=[fromDate, toDate]).order_by('-date')
-    
+
     elif enableCategory:
         fromDate = request.POST.get('fromDate')
         toDate = request.POST.get('toDate')
         categorySelect = request.POST.get('categorySelect')
         transactions = Transaction.objects.filter(category__name=categorySelect)
         transactions = transactions.filter(date__range=[fromDate, toDate]).order_by('-date')
-    
+
     else:
         fromDate = request.POST.get('fromDate')
         toDate = request.POST.get('toDate')
@@ -65,3 +68,110 @@ def balance_detail(request, pk):
     account = get_object_or_404(Account, pk=pk)
     context = {'a': account, }
     return render(request, 'transactions/balance_detail.html', context)
+
+
+def new_transaction(request, pk):
+    if request.is_ajax():
+        if request.method == 'POST':
+            try:
+                t_type = request.POST['type']
+                cat = request.POST['category']
+                sub = request.POST['subcategory']
+                date = request.POST['date']
+                comment = request.POST['comment']
+                amount = request.POST['amount']
+                category = Category.objects.get(name=cat)
+                subcategory = Subcategory.objects.get(name=sub)
+                account_id = request.POST['id']
+                account = get_object_or_404(Account, pk=account_id)
+
+                if t_type == 'i':
+                    account.balance += Decimal(float(amount))
+                else:
+                    account.balance -= Decimal(float(amount))
+                account.save()
+
+                transaction = Transaction(transaction_type=t_type, category=category, subcategory=subcategory,
+                                          comment=comment, amount=amount, balance=account.balance, date=date)
+                transaction.save()
+
+                return JsonResponse({'code': '200',
+                                     'msg': 'transaction added'})
+
+            except KeyError as e:
+                pass
+        if request.method == 'GET':
+            try:
+                transaction = get_object_or_404(Transaction, pk=pk)
+                context = {'id': transaction.pk,
+                           'type': transaction.transaction_type,
+                           'category': transaction.category,
+                           'subcategory': transaction.subcategory,
+                           'date': transaction.date,
+                           'amount': transaction.amount,
+                           }
+                return JsonResponse(context)
+            except KeyError as e:
+                pass
+        else:
+            raise Http404(request)
+    else:
+        print 'new_transaction not ajax'
+        raise Http404(request)
+
+
+def update_delete_transaction(request):
+    if request.is_ajax():
+        if request.method == 'POST':
+            try:
+                t_id = request.POST['id']
+                t_type = request.POST['type']
+                cat = request.POST['category']
+                sub = request.POST['subcategory']
+                date = request.POST['date']
+                comment = request.POST['comment']
+                amount = request.POST['amount']
+                category = Category.objects.get(name=cat)
+                subcategory = Subcategory.objects.get(name=sub)
+                account_id = request.POST['idAccount']
+                account = get_object_or_404(Account, pk=account_id)
+                transaction = Transaction.objects.get(pk=t_id)
+
+                if t_type == 'i':
+                    account.balance += Decimal(float(amount))
+                else:
+                    account.balance -= Decimal(float(amount))
+                account.save()
+                transaction.transaction_type = t_type
+                transaction.category = category
+                transaction.subcategory = subcategory
+                transaction.comment = comment
+                transaction.date = date
+                transaction.save()
+
+                return JsonResponse({'code': '200',
+                                     'msg': 'transaction added'})
+
+            except KeyError as e:
+                print e.args
+                print "KeyError at update_delete_transaction POST"
+
+        if request.method == 'GET':
+            try:
+                transaction_id = request.POST['id']
+                transaction = Transaction.objects.get(pk=transaction_id)
+                transaction.delete()
+
+                return JsonResponse({'code': '200',
+                                     'msg': 'transaction deleted',
+                                     'id': transaction_id})
+
+            except KeyError as e:
+                print e.args
+                print "KeyError at update_delete_transaction GET"
+
+        else:
+            print "update_delete_transaction request.method not supported"
+            raise Http404(request)
+    else:
+        print "update_delete_transaction not ajax"
