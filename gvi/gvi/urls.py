497dae4f9d681ed466153ecc7de99ab46dfc8c48
Mike <mcwebmike@gmail.com>
2015-07-17 09:58:57 -0500

    Base tamplate links point to each index

--- gvi/gvi/urls.py
+++ gvi/gvi/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^reports/', include('reports.urls')),
-    url(r'^budgets/', include('budgets.urls')),
+    url(r'^reports/', include('reports.urls', namespace='reports', app_name='reports')),
+    url(r'^budgets/', include('budgets.urls', namespace='budgets', app_name='budgets')),
     url(r'^accounts/', include('accounts.urls', namespace='accounts', app_name='accounts')),
     url(r'^transactions/', include('transactions.urls', namespace='transactions', app_name='transactions')),
     url(r'^hubs/', include('hubs.urls', namespace='hubs', app_name='hubs')),
